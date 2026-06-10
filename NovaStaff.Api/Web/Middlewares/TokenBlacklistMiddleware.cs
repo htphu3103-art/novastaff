bbda7e05e4579a335262fb26a00ec6f152d110b1
htphu3103-art <htphu3103@gmail.com>
2026-06-10 09:38:46 +0700

    fix: remove empty Token folder declaration from csproj

--- NovaStaff.Api/Web/Middlewares/TokenBlacklistMiddleware.cs
+++ NovaStaff.Api/Web/Middlewares/TokenBlacklistMiddleware.cs
@@ -18,21 +18,12 @@ public class TokenBlacklistMiddleware
 
     public async Task InvokeAsync(HttpContext context, ITokenBlacklistService blacklist)
     {
-        // Log tất cả claims
-        foreach (var claim in context.User.Claims)
-            Console.WriteLine($"[Claim] {claim.Type} = {claim.Value}");
-
         var userIdClaim = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value
                        ?? context.User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
 
-        Console.WriteLine($"[Blacklist] userIdClaim = {userIdClaim}");
-
         if (userIdClaim != null && int.TryParse(userIdClaim, out var userId))
         {
-            var isBlacklisted = await blacklist.IsUserBlacklistedAsync(userId);
-            Console.WriteLine($"[Blacklist] userId={userId} isBlacklisted={isBlacklisted}");
-
-            if (isBlacklisted)
+            if (await blacklist.IsUserBlacklistedAsync(userId))
             {
                 context.Response.StatusCode = 401;
                 await context.Response.WriteAsJsonAsync(new { message = "Tài khoản đã bị khóa." });
--- NovaStaff.Infrastructure/Token/RedisTokenBlacklistService.cs
+++ NovaStaff.Infrastructure/Token/RedisTokenBlacklistService.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NovaStaff.Infrastructure.Cache
+namespace NovaStaff.Infrastructure.Token
 {
     public class RedisTokenBlacklistService : ITokenBlacklistService
     {
